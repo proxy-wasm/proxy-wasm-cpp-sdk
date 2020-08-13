add287faba72b54f10e3ce52a38302bcbd88994e
John Plevyak <jplevyak@gmail.com>
2020-08-13 16:50:49 -0700

    Add case default to make gcc happy.
    Signed-off-by: John Plevyak <jplevyak@gmail.com>

--- proxy_wasm_common.h
+++ proxy_wasm_common.h
@@ -70,6 +70,8 @@ inline std::string toString(WasmResult r) {
     _CASE(InternalFailure);
     _CASE(BrokenConnection);
     _CASE(Unimplemented);
+  default:
+    return "Unknown";
   }
 }
 #undef _CASE
