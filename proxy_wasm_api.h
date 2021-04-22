a1294292894c25c2716a8e63c0899085d01924e0
BITLIU <mixdeers@gmail.com>
2021-04-22 19:47:19 +0800

    Impl getRequest/ResponseBodyBufferBytes( ) in proxy_wasm_api.h
    Signed-off-by: bitliu <mixdeers@gmail.com>

--- proxy_wasm_api.h
+++ proxy_wasm_api.h
@@ -906,11 +906,11 @@ inline WasmResult setBuffer(WasmBufferType type, size_t start, size_t length, st
 }
 
 inline WasmDataPtr getResponseBodyBufferBytes(size_t start, size_t length){
-  return getBufferBytes(WasmBufferType::HttpResponseBody,start,length);
+  return getBufferBytes(WasmBufferType::HttpResponseBody, start, length);
 }
 
 inline WasmDataPtr getRequestBodyBufferBytes(size_t start, size_t length){
-  return getBufferBytes(WasmBufferType::HttpRequestBody,start,length);
+  return getBufferBytes(WasmBufferType::HttpRequestBody, start, length);
 }
 
 // HTTP
